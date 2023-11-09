d144772f40f43e46116e3fa42615c604bc8764e4
Martin Martinez <mtz.martin@proton.me>
2023-11-09 14:37:18 -0600

    Adding buttons to the row, the last one was just TEXT and a Stack, is better like this :D

--- MechanicReservation/GridButtons.swift
+++ MechanicReservation/GridButtons.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct GridButtons: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Grid {
+            GridRow(){
+                Button {
+                    //
+                } label: {
+                    VStack{
+                        Image(systemName: "person")
+                        Text("5 seats")
+                    }
+                }
+                
+                Button {
+                    //
+                } label: {
+                    VStack {
+                        Image(systemName: "bag")
+                        Text("4 bags")
+                    }
+                }
+                
+                Button {
+                    //
+                } label: {
+                    VStack {
+                        Image(systemName: "car.window.right")
+                        Text("4 doors")
+                    }
+                }
+                
+                Button {
+                    
+                } label: {
+                    VStack {
+                        Image(systemName: "engine.combustion")
+                        Text("Auto")
+                    }
+                }
+            }
+        }.padding()
     }
 }
 
