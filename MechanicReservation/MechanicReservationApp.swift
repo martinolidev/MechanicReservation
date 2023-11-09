eb7156931e5ff643223e9a3f6119abe5ce1043e1
Martin Martinez <mtz.martin@proton.me>
2023-11-09 14:12:26 -0600

    Initial Commit

---
+++ MechanicReservation/MechanicReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MechanicReservationApp.swift
+//  MechanicReservation
+//
+//  Created by Martin Martinez on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MechanicReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
