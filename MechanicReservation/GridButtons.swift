5fba753380de8296c4d30f9f4d5e602f4ab53285
Martin Martinez <mtz.martin@proton.me>
2023-11-09 14:35:36 -0600

    I finally realized how to make buttons like the ones I added, i learned about the label in a button :D

---
+++ MechanicReservation/GridButtons.swift
@@ -0,0 +1,18 @@
+//
+//  GridButtons.swift
+//  MechanicReservation
+//
+//  Created by Martin Martinez on 09/11/23.
+//
+
+import SwiftUI
+
+struct GridButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridButtons()
+}
